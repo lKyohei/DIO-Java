5f6320a63045d664cedcbf656d988855d0cf2864
Eduardo Reis <eduardo.g.reis167@gmail.com>
2024-04-05 13:23:25 -0300

    Tipos de variaveis

---
+++ meu-primeiro-projeto-java/src/TiposVariaveis.java
@@ -0,0 +1,19 @@
+public class TiposVariaveis {
+    public static void main(String[] args) {
+       
+       byte idade = 123;
+       short ano = 2021;
+       int cep = 21070333; // se começar com zero, talvez seja melhor usar outro tipo
+       long cpf = 98765432109L; // se começar com zero, talvez seja melhor usar outro tipo e terminar com L.
+       float pi = 3.14F; // tipo float tem que terminar em F
+       double salarioMinimo = 2500.30;
+        
+       int numero = 5;
+        
+       System.out.println(numero);
+
+       final double VALOR_DE_PI = 3.14;
+       
+       
+    }
+}
