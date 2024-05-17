9f53083abd1d12f62fd8af13134d4a5fdb67c0ef
Eduardo Reis <eduardo.g.reis167@gmail.com>
2024-05-17 13:09:12 -0300

    Atualizando o controle de fluxo

---
+++ Controle-Fluxo/Controle-Fluxo/src/ExemploForArray.java
@@ -0,0 +1,17 @@
+public class ExemploForArray {
+    //em arrays o indice inicia em ZERO.
+    public static void main(String[] args) {
+    String alunos[] = {"Felipe","Jonas","Judas", "Eduardo"};
+
+        //em arrays o indice de elementos se inicia em zero.
+        for (int x=0; x<alunos.length; x++){
+        System.out.println("O aluno no indice x= " + x + " é " + alunos[x]);
+        }
+        
+        for(String aluno : alunos){
+            System.out.println("O aluno no indice é: " + aluno);
+
+        }
+    }
+
+}
---
+++ Controle-Fluxo/Controle-Fluxo/src/ExemploWhile.java
@@ -0,0 +1,16 @@
+import java.util.concurrent.ThreadLocalRandom;
+public class ExemploWhile {
+    public static void main(String[] args) {
+        double mesada = 50.0;
+        while(mesada > 0){
+            double valorDoce = valorAleatorio();
+            if(valorDoce > mesada){
+                valorDoce = mesada;
+            }
+            System.out.println("Doce do valor: " + valorDoce);
+        }
+    }
+    private static double valorAleatorio(){
+        return ThreadLocalRandom.current().nextDouble(2, 8);
+    }
+}
