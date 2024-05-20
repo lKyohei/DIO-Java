43c1a2c641ff440084a38097c78f7d088a735a1a
Eduardo Reis <eduardo.g.reis167@gmail.com>
2024-05-20 13:05:40 -0300

    Tratamento e Exceções.

---
+++ meu-primeiro-projeto-java/src/SimulacaoBancaria.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;;
+public class SimulacaoBancaria {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in); 
+        double saldo = 0; 
+        boolean chave = true;
+        
+
+
+        while (chave) {
+            System.out.println("digite uma opcao\n");
+            int opcao = scanner.nextInt(); 
+            
+            switch(opcao){
+                  case 1:
+                    double deposito = scanner.nextDouble();
+                    saldo += deposito;
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                  case 2:
+                    double saque = scanner.nextDouble();
+                    if(saque > saldo){
+                      System.out.println("Saldo insuficiente.");
+                      System.out.println("Programa encerrado.");
+                      chave=false;
+                      break;
+                    }else{
+                      saldo -= saque;
+                      System.out.println("Saldo atual: " + saldo);
+                      break;
+                    }
+                  case 3:
+                    System.out.println("Saldo atual: " + saldo);
+                    break;
+                  case 0:
+                    System.out.println("Programa encerrado.");
+                    chave = false;
+                    break;
+                default:
+                    System.out.println("Opção inválida. Tente novamente."); 
+            }
+        }
+        scanner.close();
+    }
+    
+}
